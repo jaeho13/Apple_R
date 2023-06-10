de840bbff6e836893a5e156bb810f3029413da17
이재호 <111284065+jaeho13@users.noreply.github.com>
2023-06-10 20:48:48 +0900

    Add files via upload

--- BLOG/src/App.js
+++ BLOG/src/App.js
@@ -7,6 +7,7 @@ function App() {
   let post = '강남 우동 맛집'
   let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬 독학'])
   let [따봉, 따봉변경] = useState(0);
+  let [modal, setModal] = useState(false);
 
   return (
     <div className="App">
@@ -32,12 +33,15 @@ function App() {
         <h4>{글제목[1]}</h4>
         <p>2월 17일 발행</p>
       </div>
+      
       <div className="list">
-        <h4>{글제목[2]}</h4>
+        <h4 onClick={() => { setModal(!modal) }}>{글제목[2]}</h4>
         <p>2월 17일 발행</p>
       </div>
+      {
+        modal == true ? <Modal /> : null
+      }
 
-      <Modal></Modal>
 
     </div>
   );
