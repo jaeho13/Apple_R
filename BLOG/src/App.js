f8cdf1fe311e57cde76883cfc0903ef8c8c7aa42
이재호 <111284065+jaeho13@users.noreply.github.com>
2023-06-12 23:58:42 +0900

    Add files via upload

--- BLOG/src/App.js
+++ BLOG/src/App.js
@@ -6,7 +6,7 @@ function App() {
 
   let post = '강남 우동 맛집'
   let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬 독학'])
-  let [따봉, 따봉변경] = useState(0);
+  let [따봉, 따봉변경] = useState([0, 0, 0]);
   let [modal, setModal] = useState(false);
 
   return (
@@ -22,12 +22,11 @@ function App() {
       글제목변경(copy);
       }}>글 수정</button>
 
-      <div className="list">
+      {/* <div className="list">
         <h4>{글제목[0]} <span onClick={ ()=> {따봉변경(따봉+1)}}>👍</span> 
         {따봉} </h4>
         <p>2월 17일 발행</p>
       </div>
-
 
       <div className="list">
         <h4>{글제목[1]}</h4>
@@ -37,7 +36,26 @@ function App() {
       <div className="list">
         <h4 onClick={() => { setModal(!modal) }}>{글제목[2]}</h4>
         <p>2월 17일 발행</p>
-      </div>
+      </div> */}
+
+      {
+        글제목.map(function (a, i) {
+          return (
+            <div className="list">
+              <h4>
+                {글제목[i]}
+                <span onClick={() => {
+                  let copy = [...따봉];
+                  copy[i] = copy[i] + 1;
+                  따봉변경(copy)
+                }}>👍</span> {따봉[i]}
+              </h4> 
+              <p>2월 18일 발행</p>
+            </div>)
+        })
+      }
+
+
       {
         modal == true ? <Modal /> : null
       }
